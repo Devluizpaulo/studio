ce7df53b16b8d6f4a43231d400bcfb65e7621b84
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-07-28 21:07:48 +0000

    AIzaSyCnKCxRB9uQNhDsCPAQzzJoHZYZMJitVXw

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   projectId: "rgmj-6a4f2",
   appId: "1:692857213157:web:6ce4d4948c7844a62c840c",
   storageBucket: "rgmj-6a4f2.appspot.com",
-  apiKey: "AIzaSyAjrZOdzmtyIh_AOBBzf4uL5pW0m57-N2Y",
+  apiKey: "AIzaSyCnKCxRB9uQNhDsCPAQzzJoHZYZMJitVXw",
   authDomain: "rgmj-6a4f2.firebaseapp.com",
   messagingSenderId: "692857213157"
 };
