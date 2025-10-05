ff17699f8dff2df83a694b5f3fcfcd0c645fe484
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-10-05 00:37:15 +0000

    File changes

--- src/app/dashboard/financeiro/recibo/[id]/page.tsx
+++ src/app/dashboard/financeiro/recibo/[id]/page.tsx
@@ -3,5 +3,3 @@ import { ReceiptClient } from "./ReceiptClient";
 export default function ReceiptPage({ params }: { params: { id: string } }) {
     return <ReceiptClient receiptId={params.id} />
 }
-
-    
